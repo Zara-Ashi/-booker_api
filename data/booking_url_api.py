a2bf91807d425d32f569368d5a768d1b13086ab9
zarina.ashrapova <test@hdhdh>
2026-05-06 17:28:09 +0500

    init

---
+++ data/booking_url_api.py
@@ -0,0 +1,5 @@
+HEADERS_DATA = {"Content-Type": "application/json",
+                "Accept": "application/json"}
+BASE_URL = "https://restful-booker.herokuapp.com"
+AUTH = "/auth"
+BOOKING = "/booking"
